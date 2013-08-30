53eaae717767d9667cdab6496cfa30d1e940478f
gustav9797 <egusham001@5CB3270V4N.intern.hoglandet.se>
2013-08-30 08:09:15 +0200

    nu ritas block bra>.<

--- src/Chunk.cpp
+++ src/Chunk.cpp
@@ -71,21 +71,15 @@ Block *Chunk::getHighestBlock(unsigned char x, unsigned char y)
 #ifndef _SERVER
 void Chunk::Draw(long xPos, long yPos, App &app, TextureContainer &tC)
 {
-	//backgroundSprite = &(tC.getTextures("Block0.png")[0]);
 	for(long x = 0; x < CHUNKWIDTH; x++)
 	{
 		for(long y = 0; y < CHUNKHEIGHT; y++)
 		{
 			long xPosBlock = (16*xPos-16 + x)*16;
 			long yPosBlock = (16*yPos-16 + y)*16;
-			if(blockList[x][y][0].first == nullptr || isAnySeeThrough(blockList[x][y]))
-			{
-				//backgroundSprite->SetPosition((16*xPos-16 + x)*16, (16*yPos-16 + y)*16);
-				//app.Draw(*backgroundSprite);
-			}
 			for(int l = 5; l >= 0; l--)
 			{
-				if(blockList[x][y][l].first != nullptr && xPosBlock + 16 >= (app.getView().getCenter().x - 381) && xPosBlock <= (app.getView().getCenter().x + 381)&& yPosBlock + 16 >= (app.getView().getCenter().y - 256) && yPosBlock <= (app.getView().getCenter().y + 256))
+				if(blockList[x][y][l].first != nullptr && xPosBlock + 16 >= (app.getView().getCenter().x - (app.getSize().x/2)) && xPosBlock <= (app.getView().getCenter().x + (app.getSize().x/2))&& yPosBlock + 16 >= (app.getView().getCenter().y - (app.getSize().y/2)) && yPosBlock <= (app.getView().getCenter().y + (app.getSize().y/2)))
 				{
 					blockList[x][y][l].first->Draw((xPos - 1 << 4) + x << 4, (yPos - 1 << 4) + y << 4, app, tC, blockList[x][y][l].second); 
 					if(!blockList[x][y][l].first->isSeeThrough())
