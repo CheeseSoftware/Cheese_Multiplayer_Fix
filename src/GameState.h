8756e81576850a7ada9e29868e3580d668b57da5
john <john.ostkaka@gmail.com>
2013-12-31 03:44:51 +0100

    Random diskussioner med mig själv. Du får gärna gå med och argumentera!

--- src/GameState.h
+++ src/GameState.h
@@ -15,8 +15,8 @@ public:
     //virtual void Initialize()=0;
     virtual GameState *Update(App &app)=0; // ,Callbacks &callbacks)... hmm.. Kanske ska App ha callbacks?
 #ifndef _SERVER
-	virtual void EventUpdate(App &app, const sf::Event &event)=0;
-    virtual void Draw(App &app)=0;
+	virtual void EventUpdate(App &app, const sf::Event &event)=0; // ^^ samma här
+    virtual void Draw(App &app)=0; // då får draw också callbacksXD, fast "error"-callback bör ju kunna ske där med! Men varför ska man hantera error olika? tr {} catch{} finally{} FUNKAR!
 #endif
     //virtual void Shutdown()=0;
 };
