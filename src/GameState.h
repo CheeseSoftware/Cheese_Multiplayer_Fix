f78bc865bcf1738c742855b7de3b687daa12f7c2
ostkaka0 <john.ostkaka@gmail.com>
2014-01-24 09:46:53 +0100

    ..

--- src/GameState.h
+++ src/GameState.h
@@ -4,6 +4,7 @@ class GameState;
 
 #include <SFML\Graphics.hpp>
 #include <functional>
+#include <queue>
 
 #include "App.h"
 #include "MainCallbacks.h"
@@ -16,13 +17,15 @@ class GameState;
 
 class GameState
 {
+	std::queue<std::function<void(App &app, Game &game)>> invokeQueue;
 public:
 	virtual bool Load(){return true;};
-    virtual GameState *Update(App &app, Game &game)=0; // ,Callbacks &game)... hmm.. Kanske ska App ha game?
+    virtual GameState *Update(App &app, Game &game); // ,Callbacks &game)... hmm.. Kanske ska App ha game?
 #ifdef CLIENT
 	virtual void EventUpdate(App &app, Game &game, const sf::Event &event)=0; // ^^ samma här
     virtual void Draw(App &app)=0; // då får draw också gameXD, fast "error"-callback bör ju kunna ske där med! Men varför ska man hantera error olika? tr {} catch{} finally{} FUNKAR!
 #endif
+	void Invoke(std::function<void(App &app, Game &game)> function);
     //virtual void Shutdown()=0;
 };
 
