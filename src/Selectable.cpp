929c369c928c27070bf5aa34dfb255ebf5e68e20
gustav9797 <gustav9797@hotmail.se>
2014-04-23 07:39:30 +0200

    fixa så github inte surar

--- src/Selectable.cpp
+++ src/Selectable.cpp
@@ -19,45 +19,45 @@ namespace gui
 		m_texture = nullptr;
 	}
 
-		int Selectable::m_X(App &app)
+	int Selectable::m_X(App &app)
+	{
+		int x = m_localX;
+
+		if (m_positionType == PositionType::centerTop || m_positionType == PositionType::middleCenter || m_positionType == PositionType::centerDown)
 		{
-			int x = m_localX;
-
-			if (m_positionType == PositionType::centerTop || m_positionType == PositionType::middleCenter || m_positionType == PositionType::centerDown)
-			{
-				x += app.getSize().x/2 - m_width/2;
-			}
-			else if (m_positionType == PositionType::TopRight || m_positionType == PositionType::centerRight || m_positionType == PositionType::DownRight)
-			{
-				x += app.getSize().x - m_width/2;
-			}
-			return x;
+			x += app.getSize().x/2 - m_width/2;
 		}
-
-		int Selectable::m_Y(App &app)
+		else if (m_positionType == PositionType::TopRight || m_positionType == PositionType::centerRight || m_positionType == PositionType::DownRight)
 		{
-			int y = m_localY;
-
-			if (m_positionType == PositionType::centerLeft || m_positionType == PositionType::middleCenter || m_positionType == PositionType::centerRight)
-			{
-				y += app.getSize().y/2 - m_height/2;
-			}
-			else if (m_positionType == PositionType::DownLeft || m_positionType == PositionType::centerDown || m_positionType == PositionType::DownRight)
-			{
-				y += app.getSize().y - m_height/2;
-			}
-			return y;
+			x += app.getSize().x - m_width/2;
 		}
+		return x;
+	}
 
-		int Selectable::m_X2(App &app)
+	int Selectable::m_Y(App &app)
+	{
+		int y = m_localY;
+
+		if (m_positionType == PositionType::centerLeft || m_positionType == PositionType::middleCenter || m_positionType == PositionType::centerRight)
 		{
-			return m_X(app) + m_width;
+			y += app.getSize().y/2 - m_height/2;
 		}
-
-		int Selectable::m_Y2(App &app)
+		else if (m_positionType == PositionType::DownLeft || m_positionType == PositionType::centerDown || m_positionType == PositionType::DownRight)
 		{
-			return m_Y(app) + m_height;
+			y += app.getSize().y - m_height/2;
 		}
+		return y;
+	}
+
+	int Selectable::m_X2(App &app)
+	{
+		return m_X(app) + m_width;
+	}
+
+	int Selectable::m_Y2(App &app)
+	{
+		return m_Y(app) + m_height;
+	}
 
 
 	bool Selectable::getSelected()
@@ -73,9 +73,9 @@ namespace gui
 			//std::cout << "posx:" << x + getPosition().x + m_widthOffset << " posy:" << y + getPosition().y + m_heightOffset << std::endl;
 			//std::cout << "mousex:" << sf::Mouse::getPosition().x << " mousey: " << sf::Mouse::getPosition().y << std::endl;
 			/*if (sf::Mouse::getPosition(app).x >= x + getPosition().x + m_widthOffset &&
-				sf::Mouse::getPosition(app).x <= x + getPosition().x + m_widthOffset + getSize().x &&
-				sf::Mouse::getPosition(app).y >= y + getPosition().y + m_heightOffset &&
-				sf::Mouse::getPosition(app).y <= y + getPosition().y + m_heightOffset + getSize().y)*/
+			sf::Mouse::getPosition(app).x <= x + getPosition().x + m_widthOffset + getSize().x &&
+			sf::Mouse::getPosition(app).y >= y + getPosition().y + m_heightOffset &&
+			sf::Mouse::getPosition(app).y <= y + getPosition().y + m_heightOffset + getSize().y)*/
 			if (sf::Mouse::getPosition(app).x >= m_X(app) &&
 				sf::Mouse::getPosition(app).x <= m_X2(app) &&
 				sf::Mouse::getPosition(app).y >= m_Y(app) &&
@@ -183,12 +183,12 @@ namespace gui
 			onRMBPressedFunction(app, event, x, y);
 	}
 
-		void Selectable::OnRMBReleased(App &app, const sf::Event &event, float x, float y)
+	void Selectable::OnRMBReleased(App &app, const sf::Event &event, float x, float y)
 	{
 		/*if(selected)
-			Unselect();
+		Unselect();
 		else
-			selected = true;*/
+		selected = true;*/
 
 		if (onRMBPressedFunction)
 			onRMBPressedFunction(app, event, x, y);
